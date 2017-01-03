879e3b5963ffb06cb34e35170890ce22aa06e929
Robert Jones <sirbojangles495@gmail.com>
2017-01-03 16:51:40 -0500

    Removed references to redux

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { browserHistory } from 'react-router'
-import { syncHistoryWithStore } from 'react-router-redux'
 import Root from './containers/Root'
-import configureStore from './store/configureStore'
 import Relay from 'react-relay'
 
 Relay.injectNetworkLayer(
   new Relay.DefaultNetworkLayer('https://api.github.com/graphql', {
     headers: {
-      Authorization: 'Bearer <YOUR_ACCESS_TOKEN_HERE>'
+      Authorization: 'bearer 61e221c7fb48e5bf31ad2158be4c267859c4294e'
     }
   })
 );
 
-const store = configureStore()
-const history = syncHistoryWithStore(browserHistory, store)
-
 render(
-  <Root store={store} history={history} />,
+  <Root history={browserHistory}/>,
   document.getElementById('root')
 )
