bc70508d27dada7bae6674177cd9d907b0ea77b7
Robert Jones <sirbojangles495@gmail.com>
2017-01-03 15:32:07 -0500

    Commented out code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { CALL_API, Schemas } from '../middleware/api'
+/*import { CALL_API, Schemas } from '../middleware/api'
 
 export const USER_REQUEST = 'USER_REQUEST'
 export const USER_SUCCESS = 'USER_SUCCESS'
@@ -118,3 +118,4 @@ export const RESET_ERROR_MESSAGE = 'RESET_ERROR_MESSAGE'
 export const resetErrorMessage = () => ({
     type: RESET_ERROR_MESSAGE
 })
+*/
